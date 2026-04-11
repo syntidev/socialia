c593024f2a522fe513a5ade776c3bac39d0a19ea
carbolivariA <syntidev@gmail.com>
2026-04-11 00:13:24 -0400

    fix: base path /socialia/

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+	  base: '/socialia/',
       server: {
         port: 3000,
         host: '0.0.0.0',
